2588fc23cb86d7dacded372dbc74b84171f0ac62
alsebher <114057137+alsebher@users.noreply.github.com>
2022-10-03 12:21:28 +0200

    add

---
+++ TestArrays/src/principal/TestArrays.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class TestArrays {
+    public static void main(String[] args) {
+        int [] listaNum = new int[4];
+        listaNum[0] = 2;
+        listaNum[1] = 4;
+        listaNum[2] = 6;
+        listaNum[3] = 8;
+        for(int i=0;i<4;i++){
+            System.out.println(listaNum[i]);
+        }
+                
+    }
+}
